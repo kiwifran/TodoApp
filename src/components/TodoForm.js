75931589b88a4ed5bca298fd1ecda7ed683d34f3
FrankieWang <francescafranci@outlook.com>
2019-10-24 23:45:10 -0400

    finish the form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,38 +1,109 @@
 import React, { Component } from "react";
-
-import { Form, Button } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Container, Form, Button } from "react-bootstrap";
+import { connect } from "react-redux";
+import { addTodo } from "./actions/actionCreators";
 class TodoForm extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			todoName: "",
+			isTodoDone: false,
+			todoDescription: "",
+			dueDate: new Date()
+		};
+		this.formRef = React.createRef();
 	}
+	onChangeInput = e => {
+		this.setState({
+			[e.target.id]: e.target.value
+		});
+	};
+	onDateChange = date => {
+		this.setState({
+			dueDate: date
+		});
+	};
+	onChangeRadio = () => {
+		this.setState({
+			isTodoDone: !this.state.isTodoDone
+		});
+	};
+	onFormSubmit = e => {
+		e.preventDefault();
+		console.log(this.state);
+		this.formRef.reset();
+	};
 	componentDidMount() {}
 	componentDidUpdate(prevProps, prevState) {}
 	render() {
 		return (
 			<header>
-				<Form>
-					<Form.Group controlId="formBasicEmail">
-						<Form.Label>Email address</Form.Label>
-						<Form.Control type="email" placeholder="Enter email" />
-						<Form.Text className="text-muted">
-							We'll never share your email with anyone else.
-						</Form.Text>
-					</Form.Group>
-
-					<Form.Group controlId="formBasicPassword">
-						<Form.Label>Password</Form.Label>
-						<Form.Control type="password" placeholder="Password" />
-					</Form.Group>
-					<Form.Group controlId="formBasicCheckbox">
-						<Form.Check type="checkbox" label="Check me out" />
-					</Form.Group>
-					<Button variant="primary" type="submit">
-						Submit
-					</Button>
-				</Form>
+				<Container>
+					{" "}
+					<Form
+						onSubmit={this.onFormSubmit}
+						ref={form => {
+							this.formRef = form;
+						}}
+					>
+						<Form.Group controlId="todoName">
+							<Form.Label>Type in todo's name</Form.Label>
+							<Form.Control
+								onChange={this.onChangeInput}
+								type="text"
+								placeholder="Enter name"
+								value={this.state.todoName}
+							/>
+						</Form.Group>
+						<DatePicker
+							className="form-control date"
+							value={this.state.dueDate}
+							selected={this.state.dueDate}
+							onSelect={this.onDateChange}
+						/>
+						<Form.Group>
+							<Form.Check id="isTodoDone">
+								<Form.Check
+									inline
+									label="Pending"
+									type="radio"
+									name="status"
+									onChange={this.onChangeRadio}
+									checked={this.state.isTodoDone === false}
+								/>
+								<Form.Check
+									inline
+									label="Done"
+									type="radio"
+									name="status"
+									onChange={this.onChangeRadio}
+									checked={this.state.isTodoDone === true}
+								/>
+							</Form.Check>
+						</Form.Group>
+						<Form.Group controlId="todoDescription">
+							<Form.Label>More details about the todo</Form.Label>
+							<Form.Control
+								as="textarea"
+								rows="3"
+								placeholder="describe your todo"
+								value={this.todoDescription}
+								onChange={this.onChangeInput}
+							/>
+						</Form.Group>
+						<Button variant="primary" type="submit">
+							Submit
+						</Button>
+					</Form>
+				</Container>
 			</header>
 		);
 	}
 }
-export default TodoForm;
+
+export default connect(
+	null,
+	{ addTodo }
+)(TodoForm);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import rootReducer from "./components/store/reducer";
 import thunk from "redux-thunk";
+
 const store = createStore(rootReducer);
+
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
